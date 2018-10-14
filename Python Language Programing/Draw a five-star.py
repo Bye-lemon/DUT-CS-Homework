a7d194499ce871e0d9c0454fd3decdaac26a5406
Li Yingping <Bye_lemon@mail.dlut.edu.cn>
2018-10-14 13:47:04 +0800

    Create Draw a five-star.py

---
+++ Python Language Programing/Draw a five-star.py
@@ -0,0 +1,16 @@
+# Function: Draw a five-star
+
+from turtle import *
+
+pencolor("black")
+pu()
+fd(-200)
+pd()
+fillcolor("red")
+begin_fill()
+for i in range(5):
+
+    fd(400)
+    right(144)
+
+end_fill()
