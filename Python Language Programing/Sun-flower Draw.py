45ce47f2756834f80afc1deca79ae01144a2a90a
Li Yingping <Bye_lemon@mail.dlut.edu.cn>
2018-10-14 13:48:54 +0800

    Create Sun-flower Draw.py

---
+++ Python Language Programing/Sun-flower Draw.py
@@ -0,0 +1,16 @@
+# Function: Draw a sunflower
+
+from turtle import *
+
+pencolor("red")
+pu()
+fd(-200)
+pd()
+fillcolor("yellow")
+begin_fill()
+for i in range(36):
+
+    fd(400)
+    right(170)
+
+end_fill()
